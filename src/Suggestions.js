dd80be46eee7438786b30362fc38886d150bfb27
Gerard Cabrerizo <gerard.kabre@gmail.com>
2018-02-23 18:33:26 +0100

    Basic suggestions from API implemented (no clickable)

---
+++ src/Suggestions.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Layout, Card, FormLayout, TextField, DatePicker, Button } from '@shopify/polaris';
+
+import '@shopify/polaris/styles.css';
+import './App.css';
+
+class Suggestions extends Component {
+  state = {
+    suggestions: []
+  };
+
+  componentWillReceiveProps(nextProps) {
+    let { keyword } = this.props;
+    if (nextProps.keyword !== this.props.keyword) {
+      fetch(`https://api.skypicker.com/locations/?term=${keyword}&v=2&locale=en-US`)
+        .then(res => res.json())
+        .then(res => this.setState({ suggestions: res.locations }))
+        .catch(e => console.error(e));
+    }
+  }
+
+  render() {
+    const suggestionList = this.state.suggestions.map(x => <div>{x.name}</div>);
+
+    return <div>{suggestionList}</div>;
+  }
+}
+
+export default Suggestions;
